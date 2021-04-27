566a0ab649b2e0017ae1e2e41823cfd7ff3ccc5e
尹怡来 <yoonir@ais-info.co.jp>
2021-04-27 09:38:00 +0900

    Updates

---
+++ component---src-pages-404-js-f1fc16ec99b97a1dcbb0.js
@@ -0,0 +1,2 @@
+(self.webpackChunkAISLab=self.webpackChunkAISLab||[]).push([[883],{9616:function(t,u,n){"use strict";n.r(u);var e=n(7294),s=n(5734);u.default=function(){return(0,s.tZ)(e.Fragment,null,(0,s.tZ)("h1",null,"404: Not Found"),(0,s.tZ)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f1fc16ec99b97a1dcbb0.js.map
